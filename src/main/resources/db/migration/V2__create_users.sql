c00c4c56950739af9bc7d8f10b383fb898a426cb
Kareem Hassan <kareem.tensai1@gmail.com>
2026-08-23 01:57:53 +0300

    feat(schema): create users table

---
+++ src/main/resources/db/migration/V2__create_users.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users
+(
+    id                UUID         NOT NULL,
+
+    telegram_user_id  BIGINT       NOT NULL,
+
+    telegram_group_id BIGINT       NOT NULL,
+
+    username          VARCHAR(255) NOT NULL,
+
+    password          VARCHAR(255) NOT NULL,
+
+    role              VARCHAR(20)  NOT NULL,
+
+    first_name        VARCHAR(255) NOT NULL,
+
+    last_name         VARCHAR(255),
+
+    photo_url         VARCHAR(255),
+
+    created_at        TIMESTAMPTZ  NOT NULL,
+
+    updated_at        TIMESTAMPTZ  NOT NULL,
+
+    CONSTRAINT pk_users
+        PRIMARY KEY (id),
+
+    CONSTRAINT uk_users_telegram_user_id
+        UNIQUE (telegram_user_id),
+
+    CONSTRAINT uk_users_telegram_group_id
+        UNIQUE (telegram_group_id),
+
+    CONSTRAINT uk_users_username
+        UNIQUE (username)
+);
