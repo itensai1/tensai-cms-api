087ad298c0c74420ea07b808d2cfe2bfb919f3c2
Kareem Hassan <kareem.tensai1@gmail.com>
2026-08-22 13:31:10 +0300

    chore(project): initialize Spring Boot project

---
+++ src/main/java/com/tensai/cms/TensaiCmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.tensai.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensaiCmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensaiCmsApiApplication.class, args);
+	}
+
+}
