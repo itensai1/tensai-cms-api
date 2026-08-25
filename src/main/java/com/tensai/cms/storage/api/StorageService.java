f3b514a76207cc6812d686897199d81283baeb3f
Kareem Hassan <kareem.tensai1@gmail.com>
2026-08-25 22:09:53 +0300

    feat(storage): overload store method with Resource parameter version

--- src/main/java/com/tensai/cms/storage/api/StorageService.java
+++ src/main/java/com/tensai/cms/storage/api/StorageService.java
@@ -1,12 +1,16 @@
 package com.tensai.cms.storage.api;
 
 import com.tensai.cms.storage.internal.web.dto.FileDownloadInfo;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface StorageService {
     String store(MultipartFile file) throws IOException;
+
+    String store(Resource file, String contentType) throws IOException;
 
     FileDownloadInfo download(String uniqueKey);
 }
